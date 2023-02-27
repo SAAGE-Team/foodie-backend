bb4ad7a6fe463bc9986fbba2f9d3958438481974
Anglebert-Dev <anglebertsh@gmail.com>
2023-02-27 10:44:04 +0200

    project init

--- app.js
+++ app.js
@@ -2,26 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const { json } = require('express')
-const connectDB = require('./src/config/db')
-
 
 // load env variables 
 dotenv.config()
-
-// connect to database 
-connectDB()
-
-
-// routes file
-const User = require('./src/Routes/user.routes')
 
 // init.. app
 const app = express()
 app.use(cors())
 app.use(json())
-
-app.use('api/users',User)
-
 
 // setting port
 
