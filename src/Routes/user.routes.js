c15ac9b298bee778b86288abd691154448ebf75b
b-sandrine <sanrdushimimana@gmail.com>
2023-02-28 20:59:21 +0200

    Pull sandrine

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,9 +1,5 @@
 const express = require('express')
-<<<<<<< HEAD
 const { Register, UpdateUser, DeleteUser, GetAUser, Login, GetAllUser } = require('../Controllers/user.controller')
-=======
-const { Register, UpdateUser, DeleteUser, GetAUser } = require('../Controllers/user.controller')
->>>>>>> e5acdbc (user crud)
 
 const router = express.Router()
 
@@ -13,16 +9,13 @@ router
 
 router
 .route('/allusers')
-<<<<<<< HEAD
 .get(GetAllUser)
 
 router
 .route('/Login')
 .post(Login)
-=======
 .get()
 
->>>>>>> e5acdbc (user crud)
 
 router
 .route('/:id')
