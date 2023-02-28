c92a2a4741fa829840febcd7b7c809429e09c6b9
b-sandrine <sanrdushimimana@gmail.com>
2023-02-28 16:59:56 +0200

    Changes

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const { json } = require('express')
-
+const connectDB = require('./src/config/db')
 // load env variables 
 dotenv.config()
+
+
+// connect to database 
+connectDB()
+
+
+// routes file
+const User = require('./src/Routes/user.routes')
 
 // init.. app
 const app = express()
 app.use(cors())
 app.use(json())
+app.use('/api/user',User)
 
 // setting port
 
