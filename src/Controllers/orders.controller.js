a1094d069cbb83e024c945d0db0e4ba6e53c522e
b-sandrine <sanrdushimimana@gmail.com>
2023-03-08 14:48:11 +0200

    Create order function

--- src/Controllers/orders.controller.js
+++ src/Controllers/orders.controller.js
@@ -2,7 +2,17 @@ var Order = require('../Models/orders.model')
 
 exports.createOrder = async (req,res) => {
     try {
-
+        var order = Order.create(req.body)
+        if(order) {
+            res.status(201).json({
+                success: true,
+                data: order
+            })
+        }
+        res.status(400).json({
+            success: false,
+            data: "Failed to create a new order"
+        })
     }
     catch(err) {
         res.status(500).json({
