8520fc9a236523813cc656b9a26a5405203b7690
b-sandrine <sanrdushimimana@gmail.com>
2023-03-14 11:25:15 +0200

    Order model updated

--- src/Models/orders.model.js
+++ src/Models/orders.model.js
@@ -1,27 +1,40 @@
 var mongoose = require('mongoose')
 
 var orderSchema = mongoose.Schema({
-    name: {
-        type: String,
+    customer : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Products',
+            required: true
+        },
+        quantity : {
+            type : Number,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        }
+    }],
+    total: {
+        type: Number,
         required: true
     },
     status: {
         type: String,
-        required: true
-    },
-    userId : {
-        type: String,
-        required: true
-    },
-    productId: {
-        type: String,
-        required: true
+        enum: ['pending','processing','shipped','delivered'],
+        default: 'pending'
     },
     createdAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now
     }
-})
+});
 
 var orderModel = mongoose.model('Orders', orderSchema)
 
